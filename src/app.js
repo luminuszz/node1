8e9dc1ef813256d832ae0097fa71567d26dcb156
luminuszz <luminuszz43@gmail.com>
2020-04-06 22:55:55 -0300

    push

--- src/app.js
+++ src/app.js
@@ -1,33 +1,76 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
+
+const findId = (req, res, next) => {
+  const id = req.params.id
+  const repositoryIndex = repositories.findIndex((repositories) => {
+    return repositories.id === id
+  })
+  if (repositoryIndex < 0) {
+    return res.status(400).json("Repository not found")
+  }
+  req.positionId = repositoryIndex
+  console.log(req.positionId)
+  next();
+}
 
 app.use(express.json());
 app.use(cors());
 
+
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const newRepository = { title, url, techs, likes: 0, id: uuid() }
+  repositories.push(newRepository)
+  return response.json(newRepository)
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", findId, (request, response) => {
+  const { id } = request.params
+  const indexRepository = request.positionId
+  const { title, techs, url, likes } = request.body
+
+  const uptade = {
+    title, url, techs, likes: 0, id
+  }
+  repositories[indexRepository] = uptade
+  return response.json({ title, url, techs, likes: 0, id })
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", findId, (request, response) => {
+  console.log("teste")
+  const indexRepository = request.positionId
+  repositories.splice(indexRepository, 1);
+  return response.status(204).send()
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", findId, (request, response) => {
+  const indexRepository = request.positionId
+  const { id } = request.params
+  const { title, url, techs, likes } = repositories.find(repository => repository.id === id)
+
+  const uptadeLike = {
+    title,
+    url,
+    techs,
+    likes: (likes + 1),
+    id
+
+  }
+  repositories[indexRepository] = uptadeLike
+  return response.json({ likes: uptadeLike.likes })
 });
 
 module.exports = app;
